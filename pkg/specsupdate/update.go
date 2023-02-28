e2644f341c2f233313d3e7ae86cc0ca669749ace
Robert Sigler <sigler@improvisedscience.org>
2023-02-27 21:11:34 -0500

    Removing Specs struct

--- pkg/specsupdate/update.go
+++ pkg/specsupdate/update.go
@@ -87,7 +87,14 @@ func (c *SpecsConfig) Update() error {
 		return &ErrZipReader{Err: err}
 	}
 
-	specs := &gtfspec.Specs{}
+	agencies := make([]*gtfspec.Agency, 0)
+	stops := make([]*gtfspec.Stop, 0)
+	routes := make([]*gtfspec.Route, 0)
+	trips := make([]*gtfspec.Trip, 0)
+	shapes := make([]*gtfspec.Shape, 0)
+	stopTimes := make([]*gtfspec.StopTime, 0)
+	calendars := make([]*gtfspec.Calendar, 0)
+	calendarDates := make([]*gtfspec.CalendarDate, 0)
 
 	for _, file := range zipReader.File {
 		zipData, err := readZipFile(file)
@@ -104,7 +111,6 @@ func (c *SpecsConfig) Update() error {
 		switch file.Name {
 		case "agency.txt":
 			c.log.Info().Msg("parsing agency.txt")
-			specs.Agencies = make([]*gtfspec.Agency, 0)
 
 			for _, row := range csvData[1:] {
 				agency := &gtfspec.Agency{}
@@ -112,14 +118,13 @@ func (c *SpecsConfig) Update() error {
 				if err := agency.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Agencies = append(specs.Agencies, agency)
+					agencies = append(agencies, agency)
 				}
 
 			}
 
 		case "calendar.txt":
 			c.log.Info().Msg("parsing calendar.txt")
-			specs.Calendars = make([]*gtfspec.Calendar, 0)
 
 			for _, row := range csvData[1:] {
 				calendar := &gtfspec.Calendar{}
@@ -127,13 +132,12 @@ func (c *SpecsConfig) Update() error {
 				if err := calendar.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Calendars = append(specs.Calendars, calendar)
+					calendars = append(calendars, calendar)
 				}
 			}
 
 		case "calendar_dates.txt":
 			c.log.Info().Msg("parsing calendar_dates.txt")
-			specs.CalendarDates = make([]*gtfspec.CalendarDate, 0)
 
 			for _, row := range csvData[1:] {
 				calendarDate := &gtfspec.CalendarDate{}
@@ -141,106 +145,102 @@ func (c *SpecsConfig) Update() error {
 				if err := calendarDate.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				}
-				specs.CalendarDates = append(specs.CalendarDates, calendarDate)
+				calendarDates = append(calendarDates, calendarDate)
 			}
 
 		case "routes.txt":
 			c.log.Info().Msg("parsing routes.txt")
-			specs.Routes = make([]*gtfspec.Route, 0)
 
 			for _, row := range csvData[1:] {
 				route := &gtfspec.Route{}
 				if err := route.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Routes = append(specs.Routes, route)
+					routes = append(routes, route)
 				}
 			}
 
 		case "shapes.txt":
 			c.log.Info().Msg("parsing shapes.txt")
-			specs.Shapes = make([]*gtfspec.Shape, 0)
 
 			for _, row := range csvData[1:] {
 				shape := &gtfspec.Shape{}
 				if err := shape.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Shapes = append(specs.Shapes, shape)
+					//specs.Shapes = append(specs.Shapes, shape)
+					shapes = append(shapes, shape)
 				}
 			}
 
 		case "stop_times.txt":
 			c.log.Info().Msg("parsing stop_times.txt")
-			specs.StopTimes = make([]*gtfspec.StopTime, 0)
 
 			for _, row := range csvData[1:] {
 				stopTime := &gtfspec.StopTime{}
 				if err := stopTime.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.StopTimes = append(specs.StopTimes, stopTime)
+					stopTimes = append(stopTimes, stopTime)
 				}
 			}
 
 		case "stops.txt":
 			c.log.Info().Msg("parsing stops.txt")
-			specs.Stops = make([]*gtfspec.Stop, 0)
 
 			for _, row := range csvData[1:] {
 				stop := &gtfspec.Stop{}
 				if err := stop.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Stops = append(specs.Stops, stop)
+					stops = append(stops, stop)
 				}
 			}
 
 		case "trips.txt":
 			c.log.Info().Msg("parsing trips.txt")
-			specs.Trips = make([]*gtfspec.Trip, 0)
 
 			for _, row := range csvData[1:] {
 				trip := &gtfspec.Trip{}
 				if err := trip.Add(headers, row); err != nil {
 					return &ErrParsingFile{Err: err, File: file.Name}
 				} else {
-					specs.Trips = append(specs.Trips, trip)
+					trips = append(trips, trip)
 				}
 			}
 		}
 	}
 
 	c.log.Info().Msg("adding Agencies to database")
-	if _, err := c.db.Create(specs.Agencies); err != nil {
+	if _, err := c.db.Create(agencies); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Agencies"}
 	}
 	c.log.Info().Msg("adding Calendars to database")
-	if _, err := c.db.Create(specs.Calendars); err != nil {
+	if _, err := c.db.Create(calendars); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Calendars"}
 	}
 	c.log.Info().Msg("adding CalendarDates to database")
-	if _, err := c.db.Create(specs.CalendarDates); err != nil {
+	if _, err := c.db.Create(calendarDates); err != nil {
 		return &ErrAddingData{Err: err, Structure: "CalendarDates"}
 	}
 	c.log.Info().Msg("adding Routes to database")
-	if _, err := c.db.Create(specs.Routes); err != nil {
+	if _, err := c.db.Create(routes); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Routes"}
 	}
 	c.log.Info().Msg("adding Shapes to database")
-	if _, err := c.db.Create(specs.Shapes); err != nil {
+	if _, err := c.db.Create(shapes); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Shapes"}
 	}
 	c.log.Info().Msg("adding Stops to database")
-	if _, err := c.db.Create(specs.Stops); err != nil {
+	if _, err := c.db.Create(stops); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Stops"}
 	}
 	c.log.Info().Msg("adding StopTimes to database")
-	if _, err := c.db.Create(specs.StopTimes); err != nil {
+	if _, err := c.db.Create(stopTimes); err != nil {
 		return &ErrAddingData{Err: err, Structure: "StopTimes"}
 	}
 	c.log.Info().Msg("adding Trips to database")
-	if _, err := c.db.Create(specs.Trips); err != nil {
+	if _, err := c.db.Create(trips); err != nil {
 		return &ErrAddingData{Err: err, Structure: "Trips"}
 	}
 
